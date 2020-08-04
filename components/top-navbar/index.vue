63515bd2edec77c83ad7386d187fa376645732b5
Ajo John <ajo@getnuo.com>
2020-08-04 19:54:54 +0530

    :boom: fix modal ui errors

--- components/top-navbar/index.vue
+++ components/top-navbar/index.vue
@@ -58,4 +58,21 @@
     </div>
 </template>
 
-<script src="./index.js"/>
+<script>
+export default {
+    data () {
+        return {
+            menuItems: [
+                // {
+                //     name: 'Color Swatches',
+                //     active: true
+                // },
+                // {
+                //     name: 'Color Converter',
+                //     active: false
+                // }
+            ]
+        }
+    }
+}
+</script>
